b2b17ca10f7202b5587f4f45cf617fa4f64b4320
Gelato <you@example.com>
2023-03-07 22:30:21 +0300

    Созданы страницы about и help и добавлены в роуты

---
+++ src/pages/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page>
+    <h5>About US</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "About",
+});
+</script>
--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,6 +1,18 @@
 <template>
   <q-page class="flex flex-center">
     <h5>Todo</h5>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias ea non
+      animi doloremque. Molestiae voluptates corporis quis! Mollitia vero
+      reprehenderit ipsum cupiditate ex dolorum itaque, iusto ea dolores
+      expedita assumenda.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+      perspiciatis molestias voluptate mollitia amet, assumenda at neque debitis
+      voluptatum est, et sunt tempora nulla vero odit dignissimos cupiditate
+      recusandae? Laudantium.
+    </p>
   </q-page>
 </template>
 
