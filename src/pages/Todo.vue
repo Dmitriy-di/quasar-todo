050e91995beacbc6f3026a89ba72ce13fc803b16
Gelato <you@example.com>
2023-03-08 16:46:12 +0300

    Страница todo: добавлена анимаци при удалении элементов(при помощи transition-group

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,36 +1,38 @@
 <template>
   <q-page class="q-pa-lg">
     <q-list separator bordered>
-      <q-item
-        @click="task.done[0] = !task.done[0]"
-        v-for="(task, index) in tasks"
-        :key="task.title"
-        clickable
-        :class="{ 'done bg-green-2': task.done[0] }"
-        class="flex justify-between"
-      >
-        <div class="q-pa-md">
-          <q-option-group
-            :options="options"
-            type="checkbox"
-            v-model="task.done[0]"
-          />
-        </div>
-        <q-item-section class="item-title">
-          {{ task.title }}
-        </q-item-section>
-        <transition name="slide-fade">
-          <q-item-section class="items-end" v-if="task.done[0]">
-            <q-btn
-              @click.stop="deleteTask(task, index)"
-              size="15px"
-              round
-              color="teal"
-              icon="delete"
+      <transition-group name="fade" tag="div">
+        <q-item
+          @click="task.done[0] = !task.done[0]"
+          v-for="(task, index) in tasks"
+          :key="task.title"
+          clickable
+          :class="{ 'done bg-green-2': task.done[0] }"
+          class="flex justify-between"
+        >
+          <div class="q-pa-md">
+            <q-option-group
+              :options="options"
+              type="checkbox"
+              v-model="task.done[0]"
             />
+          </div>
+          <q-item-section class="item-title">
+            {{ task.title }}
           </q-item-section>
-        </transition>
-      </q-item>
+          <transition name="slide-fade">
+            <q-item-section class="items-end" v-if="task.done[0]">
+              <q-btn
+                @click.stop="deleteTask(task, index)"
+                size="15px"
+                round
+                color="teal"
+                icon="delete"
+              />
+            </q-item-section>
+          </transition>
+        </q-item>
+      </transition-group>
     </q-list>
   </q-page>
 </template>
@@ -81,7 +83,7 @@ export default defineComponent({
     color: green;
   }
 }
-//======TRANSITION=======
+//======TRANSITION slide-fade=======
 .slide-fade-enter-active {
   transition: all 0.3s ease-out;
 }
@@ -92,6 +94,20 @@ export default defineComponent({
 
 .slide-fade-enter-from,
 .slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+//======TRANSITION fade=======
+.fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.fade-enter-from,
+.fade-leave-to {
   transform: translateX(20px);
   opacity: 0;
 }
