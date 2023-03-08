ef07ff0c46e583801542c331ad04c01a1ff2737f
Gelato <you@example.com>
2023-03-08 14:02:33 +0300

    Страница todo: добавлены атрибуты separator, bordered к q-list; событие click к q-item и атрибут clickable

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,33 +1,46 @@
 <template>
-  <q-page class="flex-column q-pa-lg">
-    <q-item v-for="task in tasks" :key="task.title">
-      <div class="q-pa-md">
-        <q-option-group :options="options" type="checkbox" v-model="group" />
-      </div>
-    </q-item>
+  <q-page class="q-pa-lg">
+    <q-list separator bordered>
+      <q-item
+        @click="task.done = !task.done"
+        v-for="task in tasks"
+        :key="task.title"
+      >
+        <div class="q-pa-md">
+          <q-option-group
+            :options="options"
+            type="checkbox"
+            v-model="task.done"
+          />
+        </div>
+        <q-item-section>
+          {{ task.title }}
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 
 export default defineComponent({
   name: "Todo",
   setup() {
-    const tasks = [
+    const tasks = reactive([
       {
         title: "hello",
-        done: false,
+        done: [false],
       },
       {
         title: "hello2",
-        done: false,
+        done: [false],
       },
       {
         title: "hello3",
-        done: false,
+        done: [false],
       },
-    ];
+    ]);
     return {
       tasks,
       group: ref([]),
